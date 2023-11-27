e0e09e541c4add00f254aa2b106e8b3359b1f182
CyberKL <lotfyk29@yahoo.com>
2023-11-27 17:48:40 +0200

    Add inheritance from FootballCharacter to Player and Add fields and constructor to Player

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Player.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Player.java
@@ -1,4 +1,28 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
-public abstract class Player {
+import java.time.LocalDate;
+
+public abstract class Player extends FootballCharacter{
+
+    protected int playerId;
+    protected int number;
+    protected Team team;
+    protected int height;
+    protected int weight;
+    protected String preferredFoot;
+    protected String position;
+    private static int numOfPlayers = 0;
+
+    protected Player(String name, LocalDate dateOfBirth, String nationality, int playerId, int number, Team team, int height, int weight, String preferredFoot, String position, int yellowCards, int redCards){
+        super(name, dateOfBirth, nationality, yellowCards, redCards);
+        this.playerId = playerId;
+        this.number = number;
+        this.team = team;
+        this.height = height;
+        this.weight = weight;
+        this.preferredFoot = preferredFoot;
+        this.position = position;
+        numOfPlayers ++;
+    }
+
 }
