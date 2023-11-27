d612ff1d72e27090282623d2c66633a9c32047d0
CyberKL <lotfyk29@yahoo.com>
2023-11-27 17:24:09 +0200

    Add fields, constructor and methods to FootballCharacter class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/FootballCharacter.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/FootballCharacter.java
@@ -1,4 +1,30 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public abstract class FootballCharacter {
+
+    protected String name;
+    protected LocalDate dateOfBirth;
+    protected String nationality;
+    protected int yellowCards;
+    protected int redCards;
+    protected int age;
+
+    protected FootballCharacter(String name, LocalDate dateOfBirth, String nationality, int yellowCards, int redCards){
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+        this.yellowCards = yellowCards;
+        this.redCards = redCards;
+        calculateAge();
+    }
+
+    protected void calculateAge(){
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(dateOfBirth, currentDate);
+        this.age = period.getYears();
+    }
+
 }
