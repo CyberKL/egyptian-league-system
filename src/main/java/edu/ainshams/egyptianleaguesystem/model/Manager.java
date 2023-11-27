8948da4a3ea381d3d639b80401dbe692b2feec30
CyberKL <lotfyk29@yahoo.com>
2023-11-27 20:02:57 +0200

    Add fields and constructor (Manager class)

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Manager.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Manager.java
@@ -1,4 +1,20 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
-public class Manager {
+import java.time.LocalDate;
+
+public class Manager extends FootballCharacter{
+
+    private Team team;
+    private int trophies;
+    private boolean wasPlayer;
+    private static int numOfManagers;
+
+    public Manager(String name, LocalDate dateOfBirth, String nationality, int yellowCards, int redCards, Team team, int trophies, boolean wasPlayer){
+        super(name,dateOfBirth, nationality, yellowCards, redCards);
+        this.team = team;
+        this.trophies = trophies;
+        this.wasPlayer = wasPlayer;
+        numOfManagers++;
+    }
+
 }
