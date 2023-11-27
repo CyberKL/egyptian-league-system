90ac8a2fae6941f1e9241a0569c3d3cb15071945
CyberKL <lotfyk29@yahoo.com>
2023-11-27 19:40:20 +0200

    Add fields and constructor to Match class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Match.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Match.java
@@ -3,9 +3,20 @@ package edu.ainshams.egyptianleaguesystem.model;
 import java.time.LocalDate;
 
 public class Match {
+
     private int matchId;
     private LocalDate date;
     private  Team team;
     private Referee referee;
+    private Score score;
+    private Stadium stadium;
 
+    public Match(int matchId, LocalDate date, Team team, Referee referee, Score score, Stadium stadium) {
+        this.matchId = matchId;
+        this.date = date;
+        this.team = team;
+        this.referee = referee;
+        this.score = score;
+        this.stadium = stadium;
+    }
 }
