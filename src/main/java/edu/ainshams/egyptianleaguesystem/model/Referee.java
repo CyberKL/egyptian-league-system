4473bcec1334075dcaf5f4516404175b3ac74c91
CyberKL <lotfyk29@yahoo.com>
2023-11-27 19:20:28 +0200

    Add fields and constructor to Referee class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Referee.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Referee.java
@@ -1,4 +1,16 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
-public class Referee {
+import java.time.LocalDate;
+
+public class Referee extends FootballCharacter{
+
+    private int matchesRefereed;
+    private static int numOfReferees;
+
+    public Referee(String name, LocalDate dateOfBirth, String nationality, int yellowCards, int redCards, int matchesRefereed){
+        super(name, dateOfBirth, nationality, yellowCards, redCards);
+        this.matchesRefereed = matchesRefereed;
+        numOfReferees++;
+    }
+
 }
