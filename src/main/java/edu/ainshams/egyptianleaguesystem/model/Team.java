d10aeec0675b505d1d0fed4c1672d44bb69c117b
CyberKL <lotfyk29@yahoo.com>
2023-11-27 20:12:19 +0200

    Add fields and constructor (Team class)

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Team.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Team.java
@@ -1,4 +1,24 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
 public class Team {
+
+    private String name;
+    private int teamId;
+    private Player[] players;
+    private Player captain;
+    private Manager manager;
+    private  Match[] matches;
+    private int matchesPlayed;
+    private int totalScore;
+    private  static int numOfTeams = 0;
+
+    public Team(String name, int teamId, Player captain, Manager manager) {
+        this.name = name;
+        this.teamId = teamId;
+        this.captain = captain;
+        this.manager = manager;
+        this.matchesPlayed = 0;
+        this.totalScore = 0;
+        numOfTeams++;
+    }
 }
