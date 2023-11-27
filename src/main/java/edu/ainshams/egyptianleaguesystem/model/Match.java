33d9f9ad0014312d2855ce88aa61e4dc1a1c65c3
CyberKL <lotfyk29@yahoo.com>
2023-11-27 19:28:04 +0200

    Add some fields to Match class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Match.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Match.java
@@ -1,4 +1,11 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
+import java.time.LocalDate;
+
 public class Match {
+    private int matchId;
+    private LocalDate date;
+    private  Team team;
+    private Referee referee;
+
 }
