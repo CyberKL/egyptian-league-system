78fbae2b2d37ee866ead6503a7f84111c1acf036
CyberKL <lotfyk29@yahoo.com>
2023-11-27 19:34:18 +0200

    Add fields, constructor and toString method to Score class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Score.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Score.java
@@ -1,4 +1,18 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
 public class Score {
+
+    private int homeTeam;
+    private int awayTeam;
+
+    public Score(int homeTeam, int awayTeam){
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    @Override
+    public String toString(){
+        return homeTeam+":"+awayTeam;
+    }
+
 }
