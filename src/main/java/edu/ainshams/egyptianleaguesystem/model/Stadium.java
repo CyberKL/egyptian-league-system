88d9631b14aa40baeb6ef3818de9a7876bf047d1
CyberKL <lotfyk29@yahoo.com>
2023-11-27 19:12:45 +0200

    Add fields and constructor to Stadium class

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Stadium.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Stadium.java
@@ -1,4 +1,18 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
 public class Stadium {
+
+    private final String name;
+    private final int capacity;
+    private final String city;
+
+    private int matchesPlayedOn;
+
+    public  Stadium(String name, int capacity, String city){
+        this.name = name;
+        this.capacity = capacity;
+        this.city = city;
+        this.matchesPlayedOn = 0;
+    }
+
 }
