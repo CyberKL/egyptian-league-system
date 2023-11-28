406c9c521c23f44a526944ae15e1762967c558dc
nasr <mohamednasr200417@gmail.com>
2023-11-28 22:04:43 +0200

    Add displayRefereeInfo (Referee class)

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Referee.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Referee.java
@@ -13,4 +13,8 @@ public class Referee extends FootballCharacter{
         numOfReferees++;
     }
 
+    public String displayRefereeInfo(){
+        return "Name: "+name+"/nAge: "+age+"/nNationality: " +nationality+"/nMatches Refereed: " +matchesRefereed+ "/nYellow Cards: " +yellowCards+ "/nRed Cards: " +redCards;
+    }
+
 }
