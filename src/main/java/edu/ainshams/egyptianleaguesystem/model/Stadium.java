2f8ddd067fba04842093d284783c61b55caa1728
CyberKL <lotfyk29@yahoo.com>
2023-11-29 21:36:50 +0200

    Change upcomingMatches array to an ArrayList and Add getter and addUpcomingMatch methods for upcomingMatch field (Stadium class)

--- src/main/java/edu/ainshams/egyptianleaguesystem/model/Stadium.java
+++ src/main/java/edu/ainshams/egyptianleaguesystem/model/Stadium.java
@@ -1,11 +1,13 @@
 package edu.ainshams.egyptianleaguesystem.model;
 
+import java.util.ArrayList;
+
 public class Stadium {
 
     private final String name;
     private final int capacity;
     private final String city;
-
+    private ArrayList<Match> upcomingMatches;
     private int matchesPlayedOn;
 
     public  Stadium(String name, int capacity, String city){
@@ -13,6 +15,14 @@ public class Stadium {
         this.capacity = capacity;
         this.city = city;
         this.matchesPlayedOn = 0;
+    }
+
+    public ArrayList<Match> getUpComingMatches() {
+        return upcomingMatches;
+    }
+
+    public void addUpcomingMatch(Match upComingMatch) {
+        upcomingMatches.add(upComingMatch);
     }
 
 }
